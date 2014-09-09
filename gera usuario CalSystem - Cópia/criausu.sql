1e9476e18ac70e8ec908efb4ea67d5eceb554e65
tonykiyo <tonyhashimoto@live.com>
2014-09-09 02:44:19 -0300

    projeto
    teste

---
+++ gera usuario CalSystem - Cópia/criausu.sql
@@ -0,0 +1,5 @@
+drop user calsystem cascade;
+create user calsystem identified by calsystem default tablespace users temporary tablespace temp;
+grant dba to calsystem;
+exit
+
---
+++ gera usuario CalSystem - Cópia/gera.bat
@@ -0,0 +1,5 @@
+/*exp calsystem/calsystem file=scott.dmp
+
+sqlplus system/unipar @criausu.sql
+
+imp calsystem/calsystem file=scott.dmp full=y
